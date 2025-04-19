af02a6f135dbfb26b88dac3d50a4b5d594e68527
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-19 10:54:29 +0530

    BookAuthor pivot table model added

--- backend/models.py
+++ backend/models.py
@@ -30,9 +30,21 @@ class Book(models.Model):
     category = models.ForeignKey(Genre, on_delete=models.CASCADE)
     publication_date = models.DateField()
     copies_owned = models.IntegerField()
+    authors = models.ManyToManyField('Author', through='BookAuthor')
 
     def __str__(self):
         return self.title
 
     class Meta:
         db_table = "book"
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'book_author'
+        unique_together = ('book', 'author')  # prevents duplicate author entries for a book
+
+    def __str__(self):
+        return f"{self.author} - {self.book}"
