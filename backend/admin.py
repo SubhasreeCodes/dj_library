ff8c90019496620627ff90fe96df8fc3e6089136
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-19 10:46:47 +0530

    Author Added to Admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from backend.models import Genre
+from backend.models import Genre, Author
 
 # Register your models here.
 admin.site.register(Genre)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name',)
