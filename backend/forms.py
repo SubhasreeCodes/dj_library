9057a99bfa6388f7912738322e01aa7f2dda3803
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-19 11:05:03 +0530

    Custom User Logic Implemented

---
+++ backend/forms.py
@@ -0,0 +1,14 @@
+from backend.models import CustomUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
