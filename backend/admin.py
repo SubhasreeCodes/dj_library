cb1a6ca221b0e30e96a287ad7ca0735d625cf103
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-19 11:10:00 +0530

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+from backend.forms import CustomUserCreationForm, CustomUserChangeForm
+from backend.models import CustomUser
+from django.utils.html import format_html
+
 from backend.models import Genre, Author, Book, BookAuthor
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'gender', 'image_tag', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'gender', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'gender', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    def image_tag(self, obj):
+        return format_html('<img src ="{}" width ="150" height="150" />'.format(obj.image.url))
+    image_tag.short_description = 'Image'
+
 admin.site.register(Genre)
 
 @admin.register(Author)
@@ -17,9 +48,6 @@ class BookAuthorInline(admin.TabularInline):
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'publication_date', 'copies_owned')
-
     inlines = [BookAuthorInline]
-
     search_fields = ('title',)
-
     list_filter = ('category', 'publication_date')
