3e8f9c32dabc4860aa76241324618a3464f3ea29
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-21 19:38:51 +0530

    fines_view logic added.

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from frontend.views import home, member_register, member_login, member_logout, member_dashboard, books_list, loaned_books
+from frontend.views import home, member_register, member_login, member_logout, member_dashboard, books_list, loaned_books, fines_view
 urlpatterns = [
     path('', home),
     path('books_list/', books_list, name='books_list'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', member_login, name='login'),
     path('logout/', member_logout, name='logout'),
     path('loans/', loaned_books, name='loaned_books'),
+    path('fines/', fines_view, name='fines'),
 ]
