93fc52c45d0521b28f9d3e9b70af7a56dba744fc
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-21 12:54:43 +0530

    member_register, member_login logic added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from frontend.views import home
+from frontend.views import home, member_register, member_login
 urlpatterns = [
     path('', home),
+    path('register/', member_register, name='register'),
+    path('login/', member_login, name='login'),
 ]
