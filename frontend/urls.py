e04de893080318b6bfa35b287c3359ad7e6c0f1f
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-21 19:20:00 +0530

    member_dashboard logic added.

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from frontend.views import home, member_register, member_login, member_logout
+from frontend.views import home, member_register, member_login, member_logout, member_dashboard
 urlpatterns = [
     path('', home),
+    path('', member_dashboard, name='member_dashboard'),
     path('register/', member_register, name='register'),
     path('login/', member_login, name='login'),
     path('logout/', member_logout, name='logout'),
