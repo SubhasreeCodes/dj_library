eee636421418b1537fec438b70c087e729d02430
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-21 19:27:15 +0530

    books_list logic added.

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from frontend.views import home, member_register, member_login, member_logout, member_dashboard
+from frontend.views import home, member_register, member_login, member_logout, member_dashboard, books_list
 urlpatterns = [
     path('', home),
+    path('books_list/', books_list, name='books_list'),
     path('', member_dashboard, name='member_dashboard'),
     path('register/', member_register, name='register'),
     path('login/', member_login, name='login'),
